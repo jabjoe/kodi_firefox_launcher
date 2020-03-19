3c4a36e052bf5321b9838cae7912b5456b3f1852
Joe Burmeister <joe.burmeister@devtank.co.uk>
2020-03-19 22:00:08 +0000

    Catch firefox output to logs.

--- addon.py
+++ addon.py
@@ -8,8 +8,9 @@ addonname   = addon.getAddonInfo('name')
 
 ok = xbmcgui.Dialog().ok(addonname, "Start Firefox?")
 if ok == 'Ok':
-    devnull = open(os.devnull, 'wb')
-    p = subprocess.Popen(['/usr/bin/firefox'], stdout=devnull, stderr=devnull)
+    p = subprocess.Popen(['/usr/bin/firefox'],
+                         stdout=open("/tmp/firefox.stdout", 'wb'),
+                         stderr=open("/tmp/firefox.stderr", 'wb'))
     while p.poll() is None:
         # Sleep for 0.1 seconds
         xbmc.sleep(100000)
