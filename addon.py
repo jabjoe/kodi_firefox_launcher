46343ea32cdffbc0f6d990029983c3600a2eeaad
Joe Burmeister <joe.burmeister@devtank.co.uk>
2020-04-06 12:58:43 +0100

    Use current window's size as kiosk requires window manager.

--- addon.py
+++ addon.py
@@ -9,6 +9,9 @@ addonname   = addon.getAddonInfo('name')
 
 ok = xbmcgui.Dialog().ok(addonname, "Start Firefox?")
 if ok:
+    win = xbmcgui.Window(xbmcgui.getCurrentWindowId())
+    width       = win.getWidth()
+    height      = win.getHeight()
     xbmc.log("Starting Firefox", xbmc.LOGNOTICE)
     devnull = open(os.devnull, 'wb')
-    subprocess.Popen(['/usr/bin/firefox', '--kiosk'], stdout=devnull, stderr=devnull)
+    subprocess.Popen(['/usr/bin/firefox', '-width', str(width), '-height', str(height)], stdout=devnull, stderr=devnull)
