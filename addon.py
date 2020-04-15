ac68086eb93925c822cb9c3506583f1e95744c87
Joe Burmeister <joe.burmeister@devtank.co.uk>
2020-04-15 20:56:17 +0100

    Protect against different firefox install bins and collect leftover zombies.

--- addon.py
+++ addon.py
@@ -1,4 +1,5 @@
 import os
+import psutil
 import subprocess
 import xbmc
 import xbmcaddon
@@ -7,11 +8,27 @@ import xbmcgui
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
 
+
+def which(filename):
+    pathenv=os.getenv('PATH')
+    for p in pathenv.split(os.path.pathsep):
+        p = os.path.join(p, filename)
+        if os.path.exists(p) and os.access(p,os.X_OK):
+            return p
+
+firefox_path = which("firefox")
+
 ok = xbmcgui.Dialog().ok(addonname, "Start Firefox?")
 if ok:
+    # Collect any zombies children.
+    for p in psutil.Process().children():
+        if p.exe() == firefox_path:
+            p.terminate()
+            p.wait()
+
     win = xbmcgui.Window(xbmcgui.getCurrentWindowId())
     width       = win.getWidth()
     height      = win.getHeight()
     xbmc.log("Starting Firefox", xbmc.LOGNOTICE)
     devnull = open(os.devnull, 'wb')
-    subprocess.Popen(['/usr/bin/firefox', '-width', str(width), '-height', str(height)], stdout=devnull, stderr=devnull)
+    subprocess.Popen([firefox_path, '-width', str(width), '-height', str(height)], stdout=devnull, stderr=devnull)
