e360e676c74aa510cd340788ed31aee2a6fe7e84
Joe Burmeister <joe.burmeister@devtank.co.uk>
2020-04-15 21:22:37 +0100

    Better acturally working collecting of previous zombie instance.

--- addon.py
+++ addon.py
@@ -1,5 +1,5 @@
 import os
-import psutil
+import signal
 import subprocess
 import xbmc
 import xbmcaddon
@@ -16,19 +16,33 @@ def which(filename):
         if os.path.exists(p) and os.access(p,os.X_OK):
             return p
 
+firefox_pid = "/tmp/firefox.pid"
 firefox_path = which("firefox")
 
 ok = xbmcgui.Dialog().ok(addonname, "Start Firefox?")
 if ok:
     # Collect any zombies children.
-    for p in psutil.Process().children():
-        if p.exe() == firefox_path:
-            p.terminate()
-            p.wait()
+    if os.path.exists(firefox_pid):
+        with open(firefox_pid, "r") as f:
+            pid = int(f.readline())
+        proc_filename = "/proc/%u/stat" % pid
+        if os.path.exists(proc_filename):
+            with open(proc_filename) as f:
+                stat = f.readline().split()
+                name = stat[1]
+                state = stat[2]
+                if name == "(firefox)" and state == "Z":
+                    os.waitpid(pid, 0)
+        os.unlink(firefox_pid)
 
     win = xbmcgui.Window(xbmcgui.getCurrentWindowId())
     width       = win.getWidth()
     height      = win.getHeight()
     xbmc.log("Starting Firefox", xbmc.LOGNOTICE)
     devnull = open(os.devnull, 'wb')
-    subprocess.Popen([firefox_path, '-width', str(width), '-height', str(height)], stdout=devnull, stderr=devnull)
+    p = subprocess.Popen([firefox_path, '-width', str(width), '-height', str(height)], stdout=devnull, stderr=devnull)
+    if p:
+        with open(firefox_pid, "w") as f:
+            f.write("%u" % p.pid)
+    else:
+        xbmc.log("Failed to start Firefox", xbmc.LOGERROR)
